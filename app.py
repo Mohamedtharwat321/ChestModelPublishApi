9558cfd4cb0d8813712e61d08eb45bbab164f5e4
Mohamedtharwat321 <mohamedtharwat313@gmail.com>
2024-12-07 00:17:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ async def predict(file: UploadFile = File(...)):
 # Add this block to bind to the correct host and port
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))  # Default to port 8000 if PORT is not set
+    port = int(os.environ.get("PORT", 10000))  # Default to port 8000 if PORT is not set
     uvicorn.run(app, host="0.0.0.0", port=port)
